57e7b0e6d0e79012041823bd08c417dc7bdbad56
Skomorac <admir.skomorac@outlook.com>
2024-07-31 15:23:19 +0200

    added mutation to create new category as a troubleshooting step

--- backend/GraphQL/Mutation.php
+++ backend/GraphQL/Mutation.php
@@ -6,6 +6,7 @@ use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Definition\ObjectType;
 use App\GraphQL\Types\OrderInputType;
 use App\GraphQL\Resolvers\OrdersResolver;
+use App\GraphQL\Resolvers\CategoriesResolver;
 use GraphQL\Error\Error;
 use Exception;
 
@@ -26,6 +27,21 @@ class Mutation
                             return OrdersResolver::store($args['OrderInput']);
                         } catch (Exception $e) {
                             error_log('Error in placeOrder mutation: ' . $e->getMessage());
+                            error_log('Stack trace: ' . $e->getTraceAsString());
+                            return new Error('Server error: ' . $e->getMessage());
+                        }
+                    },
+                ],
+                'addCategory' => [
+                    'type' => Type::string(),
+                    'args' => [
+                        'name' => Type::nonNull(Type::string()),
+                    ],
+                    'resolve' => function ($rootValue, array $args) {
+                        try {
+                            return CategoriesResolver::addCategory($args['name']);
+                        } catch (Exception $e) {
+                            error_log('Error in addCategory mutation: ' . $e->getMessage());
                             error_log('Stack trace: ' . $e->getTraceAsString());
                             return new Error('Server error: ' . $e->getMessage());
                         }
--- backend/GraphQL/Resolvers/CategoriesResolver.php
+++ backend/GraphQL/Resolvers/CategoriesResolver.php
@@ -4,7 +4,6 @@ namespace App\GraphQL\Resolvers;
 
 use App\Models\Category;
 use Exception;
-use Throwable;
 
 class CategoriesResolver
 {
@@ -16,6 +15,20 @@ class CategoriesResolver
             return $categories;
         } catch (Exception $e) {
             error_log('Error in CategoriesResolver::index: ' . $e->getMessage());
+            error_log('Stack trace: ' . $e->getTraceAsString());
+            throw $e;
+        }
+    }
+
+    public static function addCategory(string $name): string
+    {
+        try {
+            $category = new Category();
+            $category->name = $name;
+            $category->save();
+            return "Category added successfully";
+        } catch (Exception $e) {
+            error_log('Error in CategoriesResolver::addCategory: ' . $e->getMessage());
             error_log('Stack trace: ' . $e->getTraceAsString());
             throw $e;
         }
